c78d2800db96d406e806e44d82daedfafe450137
LEO <leoh27.77@gmail.com>
2019-06-15 13:50:56 -0500

    Hello World
    Show a Hello world implementing a servlet

---
+++ src/main/java/edu/udem/java2/ejemplo1/vo/Persona.java
@@ -0,0 +1,15 @@
+
+package edu.udem.java2.ejemplo1.vo;
+
+/**
+ *
+ * @author s206e20
+ */
+public class Persona {
+    
+    String nombre;
+    String apellidos;
+    String usuario;
+    String password;
+    
+}
