394faf81e190dc9fcca226944aef22990c5cf919
LEO <leoh27.77@gmail.com>
2019-06-22 11:15:18 -0500

    Con formulario en método DoGet
    Se incluye saludo "Welcome!!" solicitandolo por medio de la URL:
    http://localhost:8084/sampleServlets/, y el formulario se incluye en el
    métod DoGet por mesio del recurso "/FirstServlet", url: http://localhost:8084/sampleServlets/FirstServlet

--- src/main/java/edu/udem/java2/ejemplo1/sampleservlets/FirstServlet.java
+++ src/main/java/edu/udem/java2/ejemplo1/sampleservlets/FirstServlet.java
@@ -35,12 +35,13 @@ public class FirstServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet FirstServlet</title>");            
+            out.println("<title>Servlet FirstServlet</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet FirstServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
+
         }
     }
 
@@ -56,31 +57,135 @@ public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>\n"
+                    + "<html>\n"
+                    + "<head>\n"
+                    + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+                    + "<style>\n"
+                    + "body {\n"
+                    + "  font-family: Arial, Helvetica, sans-serif;\n"
+                    + "  background-color: black;\n"
+                    + "}\n"
+                    + "\n"
+                    + "* {\n"
+                    + "  box-sizing: border-box;\n"
+                    + "}\n"
+                    + "\n"
+                    + "/* Add padding to containers */\n"
+                    + ".container {\n"
+                    + "  padding: 16px;\n"
+                    + "  background-color: white;\n"
+                    + "}\n"
+                    + "\n"
+                    + "/* Full-width input fields */\n"
+                    + "input[type=text], input[type=password] {\n"
+                    + "  width: 100%;\n"
+                    + "  padding: 15px;\n"
+                    + "  margin: 5px 0 22px 0;\n"
+                    + "  display: inline-block;\n"
+                    + "  border: none;\n"
+                    + "  background: #f1f1f1;\n"
+                    + "}\n"
+                    + "\n"
+                    + "input[type=text]:focus, input[type=password]:focus {\n"
+                    + "  background-color: #ddd;\n"
+                    + "  outline: none;\n"
+                    + "}\n"
+                    + "\n"
+                    + "/* Overwrite default styles of hr */\n"
+                    + "hr {\n"
+                    + "  border: 1px solid #f1f1f1;\n"
+                    + "  margin-bottom: 25px;\n"
+                    + "}\n"
+                    + "\n"
+                    + "/* Set a style for the submit button */\n"
+                    + ".registerbtn {\n"
+                    + "  background-color: #4CAF50;\n"
+                    + "  color: white;\n"
+                    + "  padding: 16px 20px;\n"
+                    + "  margin: 8px 0;\n"
+                    + "  border: none;\n"
+                    + "  cursor: pointer;\n"
+                    + "  width: 100%;\n"
+                    + "  opacity: 0.9;\n"
+                    + "}\n"
+                    + "\n"
+                    + ".registerbtn:hover {\n"
+                    + "  opacity: 1;\n"
+                    + "}\n"
+                    + "\n"
+                    + "/* Add a blue text color to links */\n"
+                    + "a {\n"
+                    + "  color: dodgerblue;\n"
+                    + "}\n"
+                    + "\n"
+                    + "/* Set a grey background color and center the text of the \"sign in\" section */\n"
+                    + ".signin {\n"
+                    + "  background-color: #f1f1f1;\n"
+                    + "  text-align: center;\n"
+                    + "}\n"
+                    + "</style>\n"
+                    + "</head>\n"
+                    + "<body>\n"
+                    + "\n"
+                    + "<form action=\"/action_page.php\">\n"
+                    + "  <div class=\"container\">\n"
+                    + "    <h1>Register</h1>\n"
+                    + "    <p>Please fill in this form to create an account.</p>\n"
+                    + "    <hr>\n"
+                    + "\n"
+                    + "    <label for=\"email\"><b>Email</b></label>\n"
+                    + "    <input type=\"text\" placeholder=\"Enter Email\" name=\"email\" required>\n"
+                    + "\n"
+                    + "    <label for=\"psw\"><b>Password</b></label>\n"
+                    + "    <input type=\"password\" placeholder=\"Enter Password\" name=\"psw\" required>\n"
+                    + "\n"
+                    + "    <label for=\"psw-repeat\"><b>Repeat Password</b></label>\n"
+                    + "    <input type=\"password\" placeholder=\"Repeat Password\" name=\"psw-repeat\" required>\n"
+                    + "    <hr>\n"
+                    + "    <p>By creating an account you agree to our <a href=\"#\">Terms & Privacy</a>.</p>\n"
+                    + "\n"
+                    + "    <button type=\"submit\" class=\"registerbtn\">Register</button>\n"
+                    + "  </div>\n"
+                    + "  \n"
+                    + "  <div class=\"container signin\">\n"
+                    + "    <p>Already have an account? <a href=\"#\">Sign in</a>.</p>\n"
+                    + "  </div>\n"
+                    + "</form>\n"
+                    + "\n"
+                    + "</body>\n"
+                    + "</html>");
+        }
+        }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        /**
+         * Handles the HTTP <code>POST</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doPost
+        (HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+//        processRequest(request, response);
+        }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
+        /**
+         * Returns a short description of the servlet.
+         *
+         * @return a String containing servlet description
+         */
+        @Override
+        public String getServletInfo
+        
+            () {
         return "Short description";
-    }// </editor-fold>
+        }// </editor-fold>
 
-}
+    }
